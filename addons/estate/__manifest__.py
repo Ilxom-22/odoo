9c6b637747f1dc3a60008cbf7e776491fb87e5d6
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2025-03-23 19:01:14 +0500

    Create a new empty module

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Ilkhom Karimjonov",
+    'category': 'Tutorials',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+
+    ],
+    'application': True
+}
