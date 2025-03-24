67414f90fc9b5694bc42c9c9d77a57dff087c458
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2025-03-24 11:57:55 +0500

    Add fields to the estate property model

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -4,4 +4,18 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "This is a real estate property model"
 
-    name = fields.Char()
+    name = fields.Char(required = True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection = [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
